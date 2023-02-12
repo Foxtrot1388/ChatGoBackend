f0939b927fda355982ceb01be3efb9bb4f6d33bf
Denis <foxtrot13.88@yandex.ru>
2023-02-12 15:25:25 +1000

    Продолжение добавление тестов

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -22,6 +22,8 @@ func TestAll(t *testing.T) {
 
 	validation.ErrorTag = "vall"
 	testlogin := "TestUser"
+	pass := "12345678"
+	var token string
 
 	cfg := config.Get()
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(cfg.Mongo.URI))
@@ -94,7 +96,7 @@ func TestAll(t *testing.T) {
 
 		handlerCreate := http.HandlerFunc(app.Create)
 		rec := httptest.NewRecorder()
-		req, _ := http.NewRequest("POST", "/CreateUser", strings.NewReader(fmt.Sprintf("{\"Login\":\"%s\", \"Pass\":\"12345678\"}", testlogin)))
+		req, _ := http.NewRequest("POST", "/CreateUser", strings.NewReader(fmt.Sprintf("{\"Login\":\"%s\", \"Pass\":\"%s\"}", testlogin, pass)))
 		handlerCreate.ServeHTTP(rec, req)
 
 		var NewUserAnswer app.Answer
@@ -106,6 +108,63 @@ func TestAll(t *testing.T) {
 
 		assert.Equal(t, rec.Code, http.StatusOK)
 		assert.Equal(t, createanswer, NewUserAnswer)
+	})
+
+	t.Run("Login failed", func(t *testing.T) {
+
+		handlerCreate := http.HandlerFunc(app.Create)
+		rec := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/LoginUser", strings.NewReader(fmt.Sprintf("{\"Login\":\"%s\", \"Pass\":\"87654321\"}", testlogin)))
+		handlerCreate.ServeHTTP(rec, req)
+
+		assert.Equal(t, rec.Code, http.StatusBadRequest)
+
+	})
+
+	t.Run("Login successful", func(t *testing.T) {
+
+		handlerCreate := http.HandlerFunc(app.Create)
+		rec := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/LoginUser", strings.NewReader(fmt.Sprintf("{\"Login\":\"%s\", \"Pass\":\"%s\"}", testlogin, pass)))
+		handlerCreate.ServeHTTP(rec, req)
+
+		assert.Equal(t, rec.Code, http.StatusOK)
+
+		var NewLoginAnswer app.Answer
+		err := json.NewDecoder(rec.Body).Decode(&NewLoginAnswer)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		token = NewLoginAnswer.Data.(string)
+
+	})
+
+	t.Run("Add contact successful", func(t *testing.T) {
+
+		handlerCreate := http.HandlerFunc(app.Create)
+		rec := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/AddContact", strings.NewReader(fmt.Sprintf("{\"Login\":\"%s\"}", testlogin)))
+		req.Header.Add("Authorization", "Bearer "+token)
+		handlerCreate.ServeHTTP(rec, req)
+
+		assert.Equal(t, rec.Code, http.StatusOK)
+
+		addanswer := app.Answer{
+			Error: "",
+			Data:  "Ok",
+		}
+
+		var NewAddAnswer app.Answer
+		err := json.NewDecoder(rec.Body).Decode(&NewAddAnswer)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Equal(t, addanswer, NewAddAnswer)
+
 	})
 
 }
